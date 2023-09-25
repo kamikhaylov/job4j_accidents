3f093f50616eff8945bb38c593aa3b1b72c793df
konstantin <kotmix@bk.ru>
2023-09-26 00:22:41 +0400

    0. Spring MVC [#6877]

---
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -0,0 +1,25 @@
+package ru.job4j.accidents.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Контроллер начальной страницы
+ */
+@Controller
+@AllArgsConstructor
+@RequestMapping("/accidents")
+public class IndexController {
+
+    /**
+     * Отображение начальной страницы
+     *
+     * @return начальная страница
+     */
+    @GetMapping("/index")
+    public String getIndex() {
+        return "index/index";
+    }
+}
