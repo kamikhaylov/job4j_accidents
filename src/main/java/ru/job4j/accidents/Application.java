a550dc65ae673d69514fa098a01981c1a5b171bd
konstantin <kotmix@bk.ru>
2023-09-26 00:55:06 +0400

    1. Загрузка данных в вид [#295782]

---
+++ src/main/java/ru/job4j/accidents/Application.java
@@ -0,0 +1,21 @@
+package ru.job4j.accidents;
+
+import net.jcip.annotations.ThreadSafe;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
+
+/**
+ * Запуск приложения
+ */
+@ThreadSafe
+@SpringBootApplication
+public class Application {
+    private static final Logger LOGGER = Logger.getLogger(Application.class.getName());
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        LOGGER.info("Go to http://localhost:8080/accidents/index");
+    }
+}
--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -2,6 +2,7 @@ package ru.job4j.accidents.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,7 +20,8 @@ public class IndexController {
      * @return начальная страница
      */
     @GetMapping("/index")
-    public String getIndex() {
+    public String getIndex(Model model) {
+        model.addAttribute("user", "Konstantin");
         return "index/index";
     }
 }
